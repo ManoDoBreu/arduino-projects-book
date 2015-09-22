c62bc6656b9bd9f5c207916e91053665418b2922
Jaume Sala <jaumesala@gmail.com>
2015-09-23 00:05:44 +0200

    Update comments and variable names

--- project-03/project-03.ino
+++ project-03/project-03.ino
@@ -10,20 +10,22 @@
 */
 
 // Constants (pins)
-const int   sensorPin     = A0;
-const int   firstLEDPin   = 2;
-const int   totalLEDPins  = 5;
-const float minTempValue  = 22.0;
-const float maxTempValue  = 30.0;
-const int   checkInterval = 750;
+const int   sensorPin     = A0;   // Pin where the temperature sensor is attached
+const int   firstLEDPin   = 2;    // First Digital pin used for the LED thermometer
+const int   numOfLEDs     = 5;    // Total number of LEDs used to build the thermometer
+                                  // In the book there are only 3 LEDs, I use 5 as example
+                                  // The digital pins used must be consecutive 2,3,4,5,6...
+const float minTempValue  = 22.0; // Minimum threshold temperature of the LED thermometer
+const float maxTempValue  = 30.0; // Maximum threshold temperature of the LED thermometer
+const int   checkInterval = 750;  // Time interval (in miliseconds) between readings
 
 
 /*
- * Setup LED pins as OUTPUTs and set theyr value to LOW.
- * The first pin is 2 and the las is 2+totalLEDPins
+ * Setup the LED pins as OUTPUTs and set their value to LOW.
+ * The first pin is 2 and the las is 2+numOfLEDs
  */
 void setupLEDs(){
-  for(int pinNumber = 2; pinNumber < totalLEDPins+2; pinNumber++){
+  for(int pinNumber = 2; pinNumber < numOfLEDs+2; pinNumber++){
     pinMode(pinNumber, OUTPUT);
     digitalWrite(pinNumber, LOW);
   }
@@ -32,21 +34,20 @@ void setupLEDs(){
 
 /*
  * Enable or disable LEDs according to the current temperature.
- * Using the current degrees temperature, we will turn the
- * LEDs on or f
+ * Using the current temperature, we will turn the LEDs on or off
  */
 void writeLEDs(float temperature) {
 
   // Find the range of temperature to display
   int tempDifference = maxTempValue - minTempValue;
 
-  // Find the incremental temperature
-  float tempInterval = tempDifference/ (float) totalLEDPins;
+  // Find threshold increment
+  float tempInterval = tempDifference / (float) numOfLEDs;
 
   Serial.print("|");
   
-  // Foreach pin
-  for(int nLED = 1; nLED <= totalLEDPins; nLED++){
+  // For each LED
+  for(int nLED = 1; nLED <= numOfLEDs; nLED++){
 
     // Find min threshold temperature
     float minTempLED= minTempValue + (tempInterval * (float) nLED);
@@ -103,15 +104,15 @@ void loop() {
   Serial.print("Sensor Value: ");
   Serial.print(sensorVal);
 
-  // Convert the value to Volts
-  // which is based off the reference voltage (5.0V)
+  // Convert the value to Volts which is based 
+  // off the reference voltage (5.0V)
   float voltage = (sensorVal/1024.0) * 5.0;
   
   Serial.print(", Volts: ");
   Serial.print(voltage);
   Serial.print("v");
 
-  // Find out the real temperature 
+  // Find out the real temperature in celsius
   // The resolution is 10 mV / degree centigrade with
   // a 500 mV offset to allow for negative temperatures
   // Converting from 10 mV per degree wit 500 mV offset
